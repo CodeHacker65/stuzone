f8e0bef77fdc4fcf3587dfdfd97df96c406e8421
CodeHacker65 <127602505+CodeHacker65@users.noreply.github.com>
2023-05-07 05:26:34 +0000

    main

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function parent_1(){
-    window.location="parent_1.html"
+    window.location="parent_1.html";
 }
 function school_1(){
-    window.location="school_1.html"
+    window.location="school_1.html";
+}
+function signup(){
+    window.location="select.html";
 }
