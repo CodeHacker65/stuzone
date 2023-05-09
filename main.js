b12ccc5bcdba5072db221701a04748f855e97236
CodeHacker65 <127602505+CodeHacker65@users.noreply.github.com>
2023-05-09 11:13:51 +0000

    main

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function parent_1(){
-    window.location="parent_1.html";
+function parent(){
+    window.location="parent.html";
 }
 function school_1(){
     window.location="school_1.html";
@@ -7,9 +7,32 @@ function school_1(){
 function signup(){
     window.location="signup.html";
 }
-function parent_2(){
-    window.location="parent_2.html";
+function parent_1(){
+    window.location="parent_1.html";
 }
 function index(){
     window.location="index.html";
+}
+function parent_confirm(){
+    if (document.getElementById("password").value == document.getElementById("password_conf").value){
+        window.location="parent_dashboard.html";
+    }
+    else{
+        document.getElementById("nosame").innerHTML="The passwords are not the same!!";
+    }
+}
+function passVis(){
+    var x = document.getElementById("password");
+    var y = document.getElementById("password_conf");
+    var z = document.getElementById("passVis");
+  if (x.type === "password" && y.type =="password"){
+    x.type = "text";
+    y.type = "text";
+    z.innerHTML="Hide Password";
+  }
+  else {
+    x.type = "password";
+    y.type = "password";
+    z.innerHTML="Show Password";
+  }
 }
